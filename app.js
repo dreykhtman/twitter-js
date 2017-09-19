079de2594738eba301f9460db6d62ff380420b95
Dima Reykhtman <neoxdth@gmail.com>
2017-09-19 11:30:24 -0400

    updated json

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
-const http = require('http');
+// const http = require('http');
 
 const app = express();
 //const server = http.createServer();
 
+app.use('/', function (req, res, next) {
+    console.log(req.method, res.statusCode);
+    next();
+});
+
+app.use('/special', function (req, res, next) {
+    console.log('Secret area!');
+    next();
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/news', function(req, res) {
+    res.send('News!');
 });
 
 app.listen(3000, function () {
